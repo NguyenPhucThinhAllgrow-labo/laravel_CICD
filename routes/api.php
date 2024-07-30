119299833acca6abc9b61cac32ccad536c13b9d2
Thinh Nguyen <thinh02438@gmail.com>
2024-07-30 19:44:28 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,6 @@ use App\Http\Controllers\UserController;
 // });
 
 Route::get('/user-list', [UserController::class, 'getUserList']);
+Route::get('/hello1', function (User $users) {
+    echo '1';
+});
